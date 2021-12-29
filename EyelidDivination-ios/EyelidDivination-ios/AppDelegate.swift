24fa7a3b8a4bdf7c3d7502f92bca49453e54fc2a
Jack Pan <jackpan@heyu.tw>
2021-12-29 11:37:33 +0800

    first commit

---
+++ EyelidDivination-ios/EyelidDivination-ios/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  EyelidDivination-ios
+//
+//  Created by mac on 2021/12/29.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+
+
+}
+
