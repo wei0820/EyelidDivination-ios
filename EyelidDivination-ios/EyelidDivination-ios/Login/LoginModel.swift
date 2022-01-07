13a763c905c4da4e3fd9f5d46e25c67abbccafd0
Jack Pan <jackpan@heyu.tw>
2022-01-07 17:10:46 +0800

    add password viewmodel

--- EyelidDivination-ios/EyelidDivination-ios/Login/LoginModel.swift
+++ EyelidDivination-ios/EyelidDivination-ios/Login/LoginModel.swift
@@ -13,12 +13,23 @@ class LoginModel{
 
     private let bag = DisposeBag()
     var userMail = ""
+    var userPassword = ""
+    
     
     func setUserMail(stringObservable: Observable<String>){
         stringObservable.subscribe(onNext: { string in
-            self.userMail = string
+          
         })
         .disposed(by: bag)
+        
+    }
+    
+    func setUserPassword(passwordObservable: Observable<String>){
+        passwordObservable.subscribe(onNext:{
+            
+            string  in
+            self.userPassword = string
+        }).disposed(by: bag)
         
     }
     
@@ -32,8 +43,16 @@ class LoginModel{
 //        })
 //        .disposed(by: bag)
 //    }
-
-
+//
+//    private let bag = DisposeBag()
+//    let string = BehaviorSubject<String>(value: "")
+//
+//    init() {
+//        string.asObservable().subscribe(onNext: { string in
+//            print(string)
+//        })
+//        .disposed(by: bag)
+//    }
     
     
     
