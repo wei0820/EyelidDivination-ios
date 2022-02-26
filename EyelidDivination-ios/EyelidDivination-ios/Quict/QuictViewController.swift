fe744f03607db6b5516e945e09a51cca9fc3d4bd
JackPan <jheng.wei0919@gmail.com>
2022-02-26 15:23:50 +0800

    add quict viewcontroller

---
+++ EyelidDivination-ios/EyelidDivination-ios/Quict/QuictViewController.swift
@@ -0,0 +1,19 @@
+//
+//  QuictViewController.swift
+//  EyelidDivination-ios
+//
+//  Created by JackPan on 2022/2/26.
+//
+
+import UIKit
+
+class QuictViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
