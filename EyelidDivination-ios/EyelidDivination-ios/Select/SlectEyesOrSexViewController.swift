dee8c70e3b86a7902eaf921ec015987f473f25a8
JackPan <jheng.wei0919@gmail.com>
2022-02-26 15:22:17 +0800

    add  viewController

---
+++ EyelidDivination-ios/EyelidDivination-ios/Select/SlectEyesOrSexViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SlectEyesOrSexViewController.swift
+//  EyelidDivination-ios
+//
+//  Created by JackPan on 2022/2/26.
+//
+
+import UIKit
+
+class SlectEyesOrSexViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
